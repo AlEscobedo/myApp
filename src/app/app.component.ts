6c6c829e2286f30a3f3e696c502c7bb159c44fa0
EnoJano <al.escobedo@duocuc.cl>
2024-11-27 00:53:58 -0300

    a

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,28 +13,11 @@ export class AppComponent {
   productos: any[] = [];  // Propiedad para almacenar los datos de la colección  
 
   constructor(private firestore: AngularFirestore, private baseDatosService: BaseDatosService) {
-    this.firestore.collection('Categoria').valueChanges().subscribe(data => {
-      console.log(data);  // Opcional: Muestra los datos en la consola
+    this.firestore.collection('Categoria').valueChanges().subscribe(data => {      
       this.categorias = data;  // Almacena los datos en la propiedad `categorias`
     });
     this.firestore.collection('Productos').valueChanges().subscribe(data => {
-      console.log(data);  // Opcional: Muestra los datos en la consola
       this.productos = data;  // Almacena los datos en la propiedad `categorias`
-    });
-  }
-  // Método para agregar una nueva categoría
-  agregarCategoria() {
-    if (this.nuevaCategoria.trim() === '') {
-      console.log('El nombre de la categoría no puede estar vacío');
-      return;
-    }
-
-    const categoria = { nombre: this.nuevaCategoria };  // Define la estructura de la categoría
-    this.baseDatosService.agregarCategoria(categoria).then(() => {
-      console.log('Categoría agregada con éxito');
-      this.nuevaCategoria = '';  // Limpia el campo de la nueva categoría
-    }).catch(error => {
-      console.error('Error al agregar categoría: ', error);
     });
   }
 }
